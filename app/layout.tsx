f4425ddfe6d6deb27b488e41e1698f7a8992e9a4
nafizmazlan <nafiz.mazlan@elmu.edu.my>
2026-03-19 10:58:40 +0800

    fix npm run build with 1 error at Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Lepak Gaming",
@@ -29,10 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/logo.svg" type="image/svg+xml" sizes="128x128" />
       </head>
-      <body
-        suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body suppressHydrationWarning className="antialiased">
         {children}
         <SpeedInsights />
         <Analytics />
